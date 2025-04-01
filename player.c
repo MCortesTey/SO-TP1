edbd9e60e55008e3aa7084ec75a526736ea1bf95
mothatceguy <mothatceguy@itba.edu.ar>
2025-03-31 23:05:58 -0300

    con usleep grande funciona bien..

--- player.c
+++ player.c
@@ -107,7 +107,9 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    size_t mov_count = 0;
+    
+
+    //size_t mov_count = 0;
 
     int width = atoi(argv[1]);
     IF_EXIT(width <= 0, "ancho inválido")
@@ -133,54 +135,55 @@ int main(int argc, char *argv[]) {
     }
 
     IF_EXIT(player_id == game_state->player_number, "player could not identify itself")
-    bool cut = false;
-    while (!game_state->has_finished && !game_state->players[player_id].is_blocked) {
-        sem_wait(&sync->master_access_mutex); // bloquea el acceso al estado del juego
-        sem_post(&sync->master_access_mutex); // libera el acceso al estado del juego
+    //bool cut = false;
 
+    int my_board[width*height];
+    int my_x, my_y;
+
+    unsigned char move = NONE;
+    setvbuf(stdout, NULL, _IONBF, sizeof(move)); // Desactivar el buffering de stdout
+
+    while (!game_state->has_finished && !game_state->players[player_id].is_blocked) {
+        sem_wait(&sync->master_access_mutex); 
+        sem_post(&sync->master_access_mutex);
+        
         // lee estado de juego
         sem_wait(&sync->reader_count_mutex); // bloquea el reader count
         sync->readers_count++;  // incrementa el reader count
         if (sync->readers_count == 1) { 
             sem_wait(&sync->game_state_mutex); // si hay un solo lector bloquea el game state
         }
-
+        
         sem_post(&sync->reader_count_mutex);
 
-        // chequea si el jugador está bloqueado    
-        if (game_state->players[player_id].is_blocked) { 
-            // Liberar semáforos
-            sem_post(&sync->reader_count_mutex);
-            sync->readers_count--;
-            if (sync->readers_count == 0) {
-                sem_post(&sync->game_state_mutex); // libera el game state
-            }
-            sem_post(&sync->reader_count_mutex); 
-            break; // salir del bucle si está bloqueado
-        }
+        memcpy(my_board, game_state->board_p, sizeof(int)*width*height);
+        my_x = game_state->players[player_id].x_coord;
+        my_y = game_state->players[player_id].y_coord;
 
-        // TODO: genera movimiento
 
         // libera semáforos
         sem_wait(&sync->reader_count_mutex);
-        sync->readers_count--;
-        if (sync->readers_count == 0) {
+        if (sync->readers_count-- == 1) {
             sem_post(&sync->game_state_mutex);
         }
         sem_post(&sync->reader_count_mutex);
 
-        unsigned char move = generate_move(game_state->board_width,game_state->board_height,game_state->board_p,game_state->players[player_id].x_coord,game_state->players[player_id].y_coord);
-        cut = (move == NONE);
-        // Si el movimiento generado no es NONE, escribirlo en stdout
-        if(!cut){
-            IF_EXIT(putchar(move) == EOF, "Error al escribir movimiento en stdout")
-            fflush(stdout);
-            mov_count++;
-        } else {
+        if(game_state->has_finished || game_state->players[player_id].is_blocked){
             break;
         }
-        while(!game_state->has_finished && !game_state->players[player_id].is_blocked && game_state->players[player_id].valid_mov_request + game_state->players[player_id].invalid_mov_requests != mov_count);
-        usleep(20000);
+
+        move = generate_move(width,height,my_board,my_x,my_y);
+
+        if(move == NONE || game_state->has_finished || game_state->players[player_id].is_blocked){ // estoy bloqueado
+            break;
+        }
+
+        //IF_EXIT(putchar(move) == EOF, "Error al escribir movimiento en stdout")
+        IF_EXIT(write(STDOUT_FILENO, &move, sizeof(move)) == -1, "Error al escribir movimiento en stdout")
+        //fflush(stdout);
+         
+        //while(!game_state->has_finished && !game_state->players[player_id].is_blocked && game_state->players[player_id].valid_mov_request + game_state->players[player_id].invalid_mov_requests != mov_count);
+        usleep(2000000);
     }
     // Limpieza
     if (game_state != MAP_FAILED) {
