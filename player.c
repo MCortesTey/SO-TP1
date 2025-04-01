a868b7a1eec5b06e851bb8c1b10567db5eceeba8
mothatceguy <mothatceguy@itba.edu.ar>
2025-04-01 08:57:25 -0300

    Revert "no hay que usar usleep. pendiente: hacerlo andar"
    This reverts commit 18e8aa29fbb065f581d2b8b7faccde4fb4285fb9.

--- player.c
+++ player.c
@@ -107,7 +107,9 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-   //size_t mov_count = 0;
+    
+
+    //size_t mov_count = 0;
 
     int width = atoi(argv[1]);
     IF_EXIT(width <= 0, "ancho inválido")
@@ -136,10 +138,10 @@ int main(int argc, char *argv[]) {
     //bool cut = false;
 
     int my_board[width*height];
-    
+    int my_x, my_y;
 
-    unsigned char move;
-    //setvbuf(stdout, NULL, _IONBF, sizeof(move)); // Desactivar el buffering de stdout
+    unsigned char move = NONE;
+    setvbuf(stdout, NULL, _IONBF, sizeof(move)); // Desactivar el buffering de stdout
 
     while (!game_state->has_finished && !game_state->players[player_id].is_blocked) {
         int my_x, my_y;
@@ -148,15 +150,18 @@ int main(int argc, char *argv[]) {
         
         // lee estado de juego
         sem_wait(&sync->reader_count_mutex); // bloquea el reader count
-        if (sync->readers_count++ == 0) { 
+        sync->readers_count++;  // incrementa el reader count
+        if (sync->readers_count == 1) { 
             sem_wait(&sync->game_state_mutex); // si hay un solo lector bloquea el game state
         }
+        
         sem_post(&sync->reader_count_mutex);
 
         memcpy(my_board, game_state->board_p, sizeof(int)*width*height);
         my_x = game_state->players[player_id].x_coord;
         my_y = game_state->players[player_id].y_coord;
-        
+
+
         // libera semáforos
         sem_wait(&sync->reader_count_mutex);
         if (sync->readers_count-- == 1) {
@@ -169,15 +174,17 @@ int main(int argc, char *argv[]) {
         }
 
         move = generate_move(width,height,my_board,my_x,my_y);
-        //while(!game_state->has_finished && !game_state->players[player_id].is_blocked && game_state->players[player_id].valid_mov_request + game_state->players[player_id].invalid_mov_requests != mov_count);
 
-        if(move == NONE){
+        if(move == NONE || game_state->has_finished || game_state->players[player_id].is_blocked){ // estoy bloqueado
             break;
         }
+
         //IF_EXIT(putchar(move) == EOF, "Error al escribir movimiento en stdout")
         IF_EXIT(write(STDOUT_FILENO, &move, sizeof(move)) == -1, "Error al escribir movimiento en stdout")
-        fflush(stdout);
-        //usleep(2000000);
+        //fflush(stdout);
+         
+        //while(!game_state->has_finished && !game_state->players[player_id].is_blocked && game_state->players[player_id].valid_mov_request + game_state->players[player_id].invalid_mov_requests != mov_count);
+        usleep(2000000);
     }
     // Limpieza
     if (game_state != MAP_FAILED) {
@@ -186,7 +193,6 @@ int main(int argc, char *argv[]) {
     if (sync != MAP_FAILED) {
         munmap(sync, sizeof(game_sync));
     }
-
     return EXIT_SUCCESS;
     
 }
