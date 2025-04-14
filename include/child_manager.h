2fbb846b9d0e5b5ccf7134eab2bc3fc238048c1a
mothatceguy <mothatceguy@itba.edu.ar>
2025-04-14 18:31:20 -0300

    fix: add project description and PVS-Studio reference to header and source files

--- include/child_manager.h
+++ include/child_manager.h
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 #ifndef CHILD_MANAGER_H
 #define CHILD_MANAGER_H
 
--- src/utils/sems.c
+++ src/utils/sems.c
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 #include "sems.h"
 #include "macros.h"
 #include <stdio.h>
